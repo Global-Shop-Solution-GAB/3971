579a4eb7cb0da2a8f12a7dd40f737cc70ebae3c0
Service Web <sw@gssmail.com>
2024-03-11 10:03:12 -0500

    Commit changes in C:\Websites\Git\emarlow\3971\GCG_3971_OE_Ord_Sts.sql

---
+++ GCG_3971_OE_Ord_Sts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_3971_OE_Ord_Sts"(
+ "Order_No" CHAR(7),
+ "Order_Line" CHAR(4),
+ "Part" CHAR(20),
+ "OP_Start_Date" DATE,
+ "Verbal_Promised_Dt" DATE,
+ "Priority" CHAR(3),
+ "Status" CHAR(30),
+ "Color" CHAR(30));
